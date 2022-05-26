90fb2fecd8ea88ce91b19331315607303f790ca5
Mor Khojasteh <mori.kh1987@gmail.com>
2022-05-26 17:52:41 +0100

    feat: initial setap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
