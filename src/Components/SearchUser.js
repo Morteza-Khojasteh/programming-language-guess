221f9b0091072cf1254858833ae7e9775a39b43a
Mor Khojasteh <mori.kh1987@gmail.com>
2022-05-27 22:07:58 +0100

    feat: added search form

--- src/Components/SearchUser.js
+++ src/Components/SearchUser.js
@@ -10,7 +10,31 @@ const SearchUser = () => {
         <h2 className="text-center m-4">
           GitHub user's favourite programming language!
         </h2>
+        <form autoComplete="on" >
+          <div className="row no-gutters">
+            <div className="col">
+              <input
+                className="form-control border-secondary border-right-0 rounded-0"
+                placeholder="Github Username"
+                id="example-search-input4"
+              />
+              <span
+                id="searchclear"
+                className="glyphicon glyphicon-remove-circle"
+              ></span>
+            </div>
 
+            <div className="col-auto">
+              <button
+                className="btn btn-outline-secondary border-left-0 rounded-0 rounded-right"
+                data-toggle="modal"
+                data-target="#exampleModalCenter"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
