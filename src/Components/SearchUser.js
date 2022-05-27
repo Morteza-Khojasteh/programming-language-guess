31954a39a37c9ec656632a74e4ee74552354cf51
Mor Khojasteh <mori.kh1987@gmail.com>
2022-05-27 22:05:19 +0100

    feat: added heading

--- src/Components/SearchUser.js
+++ src/Components/SearchUser.js
@@ -3,8 +3,17 @@ import React, { useState } from "react";
 const SearchUser = () => {
   const [userName, setUserName] = useState("");
   const [data, setData] = useState("");
-  
-  return (<>Hello</>);
+
+  return (
+    <>
+      <div className="container mt-5">
+        <h2 className="text-center m-4">
+          GitHub user's favourite programming language!
+        </h2>
+
+      </div>
+    </>
+  );
 };
 
 export default SearchUser;
